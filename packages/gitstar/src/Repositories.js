b3fbdaa73d107e5d485284df2cde1f31304e873f
JulianMayorga <mayorga.julian@gmail.com>
2018-03-19 11:40:27 -0300

    Allow users to load more repositories

--- packages/gitstar/src/Repositories.js
+++ packages/gitstar/src/Repositories.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
-import { Repositories, RepositoriesPlaceholder } from "gitstar-components";
+import {
+  Repositories,
+  RepositoriesPlaceholder,
+  LoadMoreButton
+} from "gitstar-components";
 
 const GET_REPOSITORIES = gql`
-  {
-    search(type: REPOSITORY, query: "language:Javascript", first: 10) {
+  query($after: String) {
+    search(
+      type: REPOSITORY
+      query: "language:Javascript"
+      first: 10
+      after: $after
+    ) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
       nodes {
         ... on Repository {
           id
@@ -28,20 +41,57 @@ class RepositoriesWrapper extends React.Component {
         </div>
       );
     }
-    return this.props.loading ? (
-      <RepositoriesPlaceholder />
-    ) : (
-      <Repositories repositories={this.props.repositories} />
+    // Show placeholders on first render
+    if (this.props.loading && !this.props.repositories) {
+      return <RepositoriesPlaceholder />;
+    }
+    // Show both repositories and placeholder when user clicks show more
+    if (this.props.loading) {
+      return (
+        <Fragment>
+          <Repositories repositories={this.props.repositories} />
+          <RepositoriesPlaceholder />
+          <LoadMoreButton loadMore={this.props.loadMore} />
+        </Fragment>
+      );
+    }
+    return (
+      <Fragment>
+        <Repositories repositories={this.props.repositories} />
+        <LoadMoreButton loadMore={this.props.loadMore} />
+      </Fragment>
     );
   }
 }
 
 export default graphql(GET_REPOSITORIES, {
-  props: ({ data: { error, loading, search } }) => {
+  props: ({ data: { error, loading, search, fetchMore } }) => {
     return {
-      repositories: search ? search.nodes : [],
+      repositories: search ? search.nodes : null,
       loading,
-      error
+      error,
+      loadMore: () =>
+        fetchMore({
+          variables: { after: search.pageInfo.endCursor },
+          updateQuery: (previousResult = {}, { fetchMoreResult = {} }) => {
+            const previousSearch = previousResult.search || {};
+            const currentSearch = fetchMoreResult.search || {};
+            const previousNodes = previousSearch.nodes || [];
+            const currentNodes = currentSearch.nodes || [];
+            // Specify how to merge new results with previous results
+            return {
+              ...previousResult,
+              search: {
+                ...previousSearch,
+                nodes: [...previousNodes, ...currentNodes],
+                pageInfo: currentSearch.pageInfo
+              }
+            };
+          }
+        })
     };
+  },
+  options: {
+    notifyOnNetworkStatusChange: true // Update loading prop after loadMore is called
   }
 })(RepositoriesWrapper);
